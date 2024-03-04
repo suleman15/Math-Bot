7a78c989456a2125c3c6e50918a233be619865f6
Suleman Ahmed <80140965+suleman15@users.noreply.github.com>
2024-03-04 11:30:25 -0800

    Update index.js

--- backend/router/index.js
+++ backend/router/index.js
@@ -3,7 +3,7 @@ import operationRouter from "./calculate.js";
 import MathBotRoute from "./mathBot.js";
 const router = express.Router();
 
-router.use("/operation", operationRouter); //working fine
-router.use("/mathbot", MathBotRoute); //working fine
+router.use("/operation", operationRouter);
+router.use("/mathbot", MathBotRoute); 
 
 export default router;
